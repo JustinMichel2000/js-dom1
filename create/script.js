f2a9e8df0351a86f0dbaa2b0cff53d8bfe7d3a49
Justin Michel <justinmichel2000@gmail.com>
2023-09-19 14:20:02 +0200

    create

--- create/script.js
+++ create/script.js
@@ -1,43 +1,59 @@
-// Modify the script.js to create a new <section> with a random background-color 
-// for each learner in your group. This section should contain a paragraph with 
-// the name of the learner. Those sections should be appended in the <article>
 
-// If the background is dark the text should be white, if the 
-// background is light the text should be black
+let learners = [
+    "Alexandre", "Antoine", "Bastien", "Carole", "Elisabeth", "Elodie",
+    "Justin", "Justine", "Kimi", "Layla", "Lidwine", "Lucas", "Mathias",
+    "Okly", "Pierre", "Robin", "Rosalie", "Stephane", "Tim", "Tom", 
+    "Valentin", "Alexandre", "Virginie"
+];
 
-// Find a way so that everytime you load the page the order of the elements changes!
+function getRandomColor() 
+{
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function getTextColor(backgroundColor) {
+
+    let rgb = backgroundColor.match(/\d+/g);
+
+    if (rgb) {
+        let brightness = 
+        (parseInt(rgb[0]) * 299 + parseInt(rgb[1]) * 587 + parseInt(rgb[2]) * 114) / 1000;
+        return brightness > 128 ? "#000" : "#fff";
+    return "#000"; 
+}
+}
 
 
-
-function addElement() {
-   
-    let newSection= document.createElement("section");
-
-    for (let elements of newSection) {
-
-        let newContent = document.createTextNode("");
-
-        newSection.appendChild(newContent);
+let article = document.querySelector("article");
 
 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
-  
+}
+
+shuffleArray(learners);
+
+
+let section = document.createElement("section");
+
+
+learners.forEach(learner => {
     
-    // add the newly created element and its content into the DOM
-    // let currentDiv = document.getElementById("div1");
-    // document.body.insertBefore(newDiv, currentDiv);
-  }
+    let paragraph = document.createElement("p");
+    paragraph.textContent = learner;
+    let backgroundColor = getRandomColor();
+    paragraph.style.backgroundColor = backgroundColor;
+    paragraph.style.color = getTextColor(backgroundColor);
+    section.appendChild(paragraph);
+});
+
+article.appendChild(section);
 
 
-
-
-
-
-
-
-["Alexandre", "Antoine", "Bastien", "Carole", 
-"Elisabeth", "Elodie", "Justin", "Justine", "Kimi",
- "Layla", "Lidwine", "Lucas", "Mathias", "Okly", "Pierre", 
- "Robin", "Rosalie", "Stephane", "Tim", "Tom", 
- "Valentin", "Alexandre", "Virginie"]
 
